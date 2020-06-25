222cdd69a22eb50ae1c12ea2d6d41e2ffc6327b3
gcp-acrhitect <praveenreddy.b@hotmail.com>
2020-06-24 22:48:17 -0400

    iac6

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ##
 provider "google" {
- credentials = file("/home/ella_anusha16/key.json")
+ credentials = file("/home/app/key.json")
  project     = "gcp-dev-276600"
  region      = "us-east1"
 }
